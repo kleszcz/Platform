b4c9f75212a35493af6c91fb6f5da293d7a5f886
Jan Kleszczyński <jankleszcz@gmail.com>
2015-01-23 21:37:59 +0100

    Spikes
    Close #2

---
+++ Assets/Scripts/Env/Spikes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PlatformEngine
+{
+	public class Spikes : MonoBehaviour
+	{
+		void OnTriggerEnter2D(Collider2D collider)
+		{
+			if (collider.tag == "Player")
+			{
+				Debug.LogWarning("DEAD!");
+				Application.LoadLevel("Menu");
+			}
+		}
+	}
+}

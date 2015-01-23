fd8a4beb1cff7790399803a16c831512c4701b4f
Jan Kleszczyński <jankleszcz@gmail.com>
2015-01-24 00:21:08 +0100

    Spikes do hurt

--- Assets/Scripts/Env/Spikes.cs
+++ Assets/Scripts/Env/Spikes.cs
@@ -2,15 +2,33 @@
 using System.Collections;
 
 namespace PlatformEngine
-{
+{	
 	public class Spikes : MonoBehaviour
 	{
+		public float damage = 1.0f;
+
 		void OnTriggerEnter2D(Collider2D collider)
 		{
 			if (collider.tag == "Player")
 			{
-				Debug.LogWarning("DEAD!");
-				Application.LoadLevel("Menu");
+				PlayerAnimation.UpdateDamage(true);
+			}
+		}
+
+		void OnTriggerExit2D(Collider2D collider)
+		{
+			if (collider.tag == "Player")
+			{
+				PlayerAnimation.UpdateDamage(false);
+			}
+		}
+		void OnTriggerStay2D(Collider2D collider)
+		{
+			if (collider.tag == "Player")
+			{
+				PlayerAnimation.UpdateDamage(true);
+				Player player = collider.GetComponent<Player>();
+				player.DoDamage(damage);
 			}
 		}
 	}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PlatformEngine
+{
+	public class Player : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		public void DoDamage(float damage)
+		{
+		}
+	}
+}

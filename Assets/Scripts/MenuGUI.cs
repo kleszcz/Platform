ecd8ac690510dc148b278bf779e8479ccc0b2174
Jan Kleszczyński <jankleszcz@gmail.com>
2015-01-23 21:38:41 +0100

    Tmp menu and camera following

---
+++ Assets/Scripts/MenuGUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGUI : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUILayout.Button("Start"))
+		{
+			Application.LoadLevel("level");
+		}
+		if (GUILayout.Button("Close"))
+		{
+			Application.Quit();
+		}
+	}
+}

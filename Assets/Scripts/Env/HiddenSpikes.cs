ed2fa6d1a62a8de42e5c24d4243299bb3d1264fb
Jan Kleszczyński <jankleszcz@gmail.com>
2015-01-26 23:22:30 +0100

    Hidden spikes. Close #3

---
+++ Assets/Scripts/Env/HiddenSpikes.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PlatformEngine
+{
+	public class HiddenSpikes : MonoBehaviour
+	{
+		[SerializeField]
+		private Animator anim;
+
+		void OnTriggerEnter2D(Collider2D collider)
+		{
+			anim.SetBool("Show", true);
+		}
+
+		void OnTriggerExit2D(Collider2D collider)
+		{
+			anim.SetBool("Show", false);
+		}
+	}
+}
